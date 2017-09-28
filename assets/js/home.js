00a7cbba27f233a2ce5000d7f937e021e951b470
John Torralba <Wet.johnt@gmail.com>
2017-09-28 16:06:21 +0000

    add jquery for hover on iframes

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,55 +1,93 @@
          // trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
          if (!String.prototype.trim) {
-            (function () {
-                // Make sure we trim BOM and NBSP
-                var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
-                String.prototype.trim = function () {
-                    return this.replace(rtrim, '');
-                };
-            })();
-        }
+             (function () {
+                 // Make sure we trim BOM and NBSP
+                 var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+                 String.prototype.trim = function () {
+                     return this.replace(rtrim, '');
+                 };
+             })();
+         }
 
-        [].slice.call(document.querySelectorAll('input.input__field')).forEach(function (inputEl) {
-            // in case the input is already filled..
-            if (inputEl.value.trim() !== '') {
-                classie.add(inputEl.parentNode, 'input--filled');
-            }
+         [].slice.call(document.querySelectorAll('input.input__field')).forEach(function (inputEl) {
+             // in case the input is already filled..
+             if (inputEl.value.trim() !== '') {
+                 classie.add(inputEl.parentNode, 'input--filled');
+             }
 
-            // events:
-            inputEl.addEventListener('focus', onInputFocus);
-            inputEl.addEventListener('blur', onInputBlur);
+             // events:
+             inputEl.addEventListener('focus', onInputFocus);
+             inputEl.addEventListener('blur', onInputBlur);
+         });
+
+         function onInputFocus(ev) {
+             classie.add(ev.target.parentNode, 'input--filled');
+         }
+
+         function onInputBlur(ev) {
+             if (ev.target.value.trim() === '') {
+                 classie.remove(ev.target.parentNode, 'input--filled');
+             }
+         }
+
+// iframes =======================================================================================================
+         // add scrolling no attribute to typeform iframe
+         function functionAddAttribute() {
+             $('iframe').each(
+                 function (index, elem) {
+                     elem.setAttribute("scrolling", "no");
+                 }
+             );
+         };
+
+         window.onload = functionAddAttribute;
+
+         //mouse enter and mouse leave functions for typeform
+         $('.typeform-widget').mouseenter(function () {
+             $('.typeform-widget iframe').each(
+                 function (index, elem) {
+                     elem.setAttribute("scrolling", "yes");
+                 }
+             )
+         });
+
+         $('.typeform-widget').mouseleave(function () {
+            $('.typeform-widget iframe').each(
+                function (index, elem) {
+                    elem.setAttribute("scrolling", "no");
+                }
+            )
         });
 
-        function onInputFocus(ev) {
-            classie.add(ev.target.parentNode, 'input--filled');
-        }
+        //mouse enter and mouse leave functions for birdeye
+        $('.birdeye-container').mouseenter(function () {
+            $('.birdeye-container iframe').each(
+                function (index, elem) {
+                    elem.setAttribute("scrolling", "yes");
+                }
+            )
+        });
 
-        function onInputBlur(ev) {
-            if (ev.target.value.trim() === '') {
-                classie.remove(ev.target.parentNode, 'input--filled');
-            }
-        }
-
-// add scrolling no attribute to typeform iframe
-    function functionAddAttribute(){
-        $("iframe").each(
-            function(index, elem) {
-                elem.setAttribute("scrolling","no");
-            }
-        );
-   };
-   
-   window.onload = functionAddAttribute;
+        $('.birdeye-container').mouseleave(function () {
+           $('.birdeye-container iframe').each(
+               function (index, elem) {
+                   elem.setAttribute("scrolling", "no");
+               }
+           )
+       });
 
 
-// youtube iframe
-// on preview show iframe
-$('#ytTrigger').on('click', function (e) {
-    var idVideo = $('#modalYtFrame').data('videoid');
-    $('#modalYtFrame').html('<iframe width="90%" height="315px" src="https://www.youtube.com/embed/' + idVideo + '?autoplay=true" frameborder="0" allowfullscreen></iframe>');
-  });
-  //on close remove
-  $('#modalClose').on('click', function () {
-     $('#modalYtFrame').empty();
-  });
-   
+        
+
+         //================================= youtube iframe 
+
+
+         // on preview show iframe
+         $('#ytTrigger').on('click', function (e) {
+             var idVideo = $('#modalYtFrame').data('videoid');
+             $('#modalYtFrame').html('<iframe width="90%" height="315px" src="https://www.youtube.com/embed/' + idVideo + '?autoplay=true" frameborder="0" allowfullscreen></iframe>');
+         });
+         //on close remove
+         $('#modalClose').on('click', function () {
+             $('#modalYtFrame').empty();
+         });
