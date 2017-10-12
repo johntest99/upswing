d86e5f0cfc7694693db9dd4100ccbce0dd5bec8a
John Torralba <Wet.johnt@gmail.com>
2017-10-12 15:23:26 +0000

    add cta3 section dynamic

--- site/snippets/sections/ctathree.php
+++ site/snippets/sections/ctathree.php
@@ -1,10 +1,13 @@
 <section class="ups-section" id="cta3">
         <div class="ups-container ups-content uk-text-center uk-container-center">
-            <p class="cta-text-3">
-                We love collaborations!
-            </p>
+            <h2 class="cta-text-3">
+                <?php echo $site->cta_three_text() ?>
+            </h2>
+            
+            <?php if($site->cta_three_button_text()->isNotEmpty()): ?>
             <div class="uk-animation-slide-bottom ups-space-up-40">
-                <a class=" ups-font-size-20 ups-button" href=""> <i class="uk-icon-phone"></i> CALL</a>
+                <a class=" ups-font-size-20 ups-button" href="tel:<?php echo $site->phone() ?>"><i class="uk-icon-phone"></i> <?php echo $site->cta_three_button_text() ?></a>
             </div>
+            <?php endif ?>
         </div>
     </section>
