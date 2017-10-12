c410719e88e435f9c3284007de8d78d8bc5e7bbe
John Torralba <Wet.johnt@gmail.com>
2017-10-12 17:29:06 +0000

    add dynamic social media icons

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,31 +2,54 @@
 <div class="ups-content uk-text-center">
     <div class="social-media">
         <ul class="social-icons">
+
+        <?php if($site->social_facebook()->isNotEmpty()){?>
             <li>
-                <a href="">
+                <a target="_blank" href="<?php echo $site->social_facebook() ?>">
                     <i class="uk-icon-facebook"></i>
                 </a>
             </li>
+        <?php }
+        if($site->social_linkedin()->isNotEmpty()){
+        ?>
             <li>
-                <a href="">
+                <a target="_blank" href="<?php echo $site->social_linkedin() ?>">
+                    <i class="uk-icon-linkedin"></i>
+                </a>
+            </li>
+        <?php } 
+        if($site->social_pinterest()->isNotEmpty()){
+        ?>
+            <li>
+                <a target="_blank" href="<?php echo $site->social_pinterest() ?>">
                     <i class="uk-icon-pinterest"></i>
                 </a>
             </li>
+        <?php } 
+        if($site->social_youtube()->isNotEmpty()){
+        ?>
             <li>
-                <a href="">
+                <a target="_blank" href="<?php echo $site->social_youtube() ?>">
                     <i class="uk-icon-youtube"></i>
                 </a>
             </li>
+        <?php } 
+        if($site->social_instagram()->isNotEmpty()){
+        ?>
             <li>
-                <a href="">
+                <a target="_blank" href="<?php echo $site->social_instagram() ?>">
                     <i class="uk-icon-instagram"></i>
                 </a>
             </li>
+        <?php } 
+        if($site->social_twitter()->isNotEmpty()){
+        ?>
             <li>
-                <a href="">
+                <a target="_blank" href="<?php echo $site->social_twitter() ?>">
                     <i class="uk-icon-twitter"></i>
                 </a>
             </li>
+        <?php } ?>
         </ul>
     </div>
 
